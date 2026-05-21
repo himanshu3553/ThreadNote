5e039d3c22aafa1701a83b471f2a0d2c1057c3b4
Himanshu Singh <singh.himanshu3535@gmail.com>
2026-05-21 19:14:38 +0530

    Fix: distribution bug

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,9 @@ const app = new App({
   appToken: process.env.SLACK_APP_TOKEN,
   socketMode: true,
   installationStore: prismaInstallationStore,
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  stateSecret: process.env.SLACK_STATE_SECRET,
 });
 
 app.event("app_mention", async (args) => {
